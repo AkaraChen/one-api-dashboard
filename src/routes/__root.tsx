594d77799c82184ce34cbdbea89bfab74791a6b5
AkaraChen <akarachen@outlook.com>
2025-06-24 11:53:22 +0800

    refactor: migrate root route to use TanStack Query and Router devtools while removing header component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,21 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-export const Route = createRootRoute({
-  component: RootComponent,
-})
+import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx'
 
-function RootComponent() {
-  return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
-  )
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
 }
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanStackQueryLayout />
+    </>
+  ),
+})
