43e184cdf462d77b22f080810d7f4f866c387f68
AkaraChen <akarachen@outlook.com>
2025-06-24 11:51:30 +0800

    refactor: simplify root layout and remove TanStack Query demo route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,15 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import * as React from 'react'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
 
-import Header from '../components/Header'
-
-import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx'
-
-import type { QueryClient } from '@tanstack/react-query'
-
-interface MyRouterContext {
-  queryClient: QueryClient
-}
-
-export const Route = createRootRouteWithContext<MyRouterContext>()({
-  component: () => (
-    <>
-      <Header />
-
-      <Outlet />
-      <TanStackRouterDevtools />
-
-      <TanStackQueryLayout />
-    </>
-  ),
+export const Route = createRootRoute({
+  component: RootComponent,
 })
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <div>Hello "__root"!</div>
+      <Outlet />
+    </React.Fragment>
+  )
+}
